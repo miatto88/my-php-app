f15582387c59a1011e66cbc82622837fc18b9a9b
miatto88 <77558705+miatto88@users.noreply.github.com>
2021-08-11 22:27:36 +0900

    Add files via upload

--- app/models/BaseModel.php
+++ app/models/BaseModel.php
@@ -3,10 +3,10 @@
 class BaseModel {
     public static function dbconnect() {    // 変更 stasticなメソッドに
         try {
-            $dbh = new PDO("host=my-php-app_mysql_1;mysql:dbname=common;charset=utf8", "root", "miatto");
-            return $dbh;
-            // $dbh = new PDO("mysql:dbname=common;host=127.0.0.1;charset=utf8", "root", "");
+            // $dbh = new PDO("host=my-php-app_mysql_1;mysql:dbname=common;charset=utf8", "root", "miatto");
             // return $dbh;
+            $dbh = new PDO("mysql:dbname=common;host=127.0.0.1;charset=utf8", "root", "");
+            return $dbh;
         } catch (PDOException $e) {
             echo "DB接続エラー: " . $e->getMessage();
         }
