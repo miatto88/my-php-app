4aec3016791537f2d336056639f3c3140d971e1b
miatto88 <77558705+miatto88@users.noreply.github.com>
2021-09-02 00:12:07 +0900

    Add files via upload

--- app/controllers/ItemController.php
+++ app/controllers/ItemController.php
@@ -10,8 +10,19 @@ class ItemController {
 
     public static function detail() {
         $item_id = $_GET["id"];
+
+        if ($item_id === null) { // 変更 $item_id が取得できない場合の処理
+            header("Location: ../error/404.php");
+            exit();
+        }
+        
         $item = Item::findById($item_id);
-        Item::isExistById($item); // 変更 id存在チェックを追加
+
+        $is_exist = Item::isExistById($item);
+        if ($is_exist === false) { // 変更 リダイレクト処理
+            header("Location: ../error/404.php");
+            exit();
+        };
 
         return $item;
     }
