321a668da3f6c6dc089437aecc5efbd56433eafa
miatto88 <miatto0788@gmail.com>
2021-09-13 17:01:16 +0900

    2021-09-13 mysqlへの接続調整

--- app/models/BaseModel.php
+++ app/models/BaseModel.php
@@ -3,10 +3,13 @@
 class BaseModel {
     public static function dbconnect() {
         try {
-            $dbh = new PDO("host=my-php-app_mysql_1;mysql:dbname=common;charset=utf8", "root", "miatto");
-            return $dbh;
+            $dsn = "mysql:host=my-php-app_mysql_1;dbname=common;charset=utf8";
+            $dbh = new PDO($dsn, "root", "miatto");
+
             // $dbh = new PDO("mysql:dbname=common;host=127.0.0.1;charset=utf8", "root", "");
             // return $dbh;
+            
+            return $dbh;
         } catch (PDOException $e) {
             echo "DB接続エラー: " . $e->getMessage();
         }
